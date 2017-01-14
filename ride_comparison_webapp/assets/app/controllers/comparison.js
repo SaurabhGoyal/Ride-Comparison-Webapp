fd17cff0bfe973b708e226ee24a89b2cf545704e
Saurabh Goyal <saurabh.2561@gmail.com>
2017-01-14 18:57:34 +0530

    integrate use of ride comparison API for data

--- ride_comparison_webapp/assets/app/controllers/comparison.js
+++ ride_comparison_webapp/assets/app/controllers/comparison.js
@@ -1,12 +1,13 @@
 app.controller("ComparisonController", ["$scope", "ComparisonService", function($scope, ComparisonService){
-    $scope.start_lat = 0.000000;
-    $scope.start_long = 0.000000;
-    $scope.end_lat = 0.000000;
-    $scope.end_long = 0.000000;
+
+    $scope.start_lat = 28.4781029;
+    $scope.start_long = 77.0493641;
+    $scope.end_lat = 28.496481;
+    $scope.end_long = 77.085328;
     $scope.rideEstimates =  null;
+
     $scope.getRideEstimates = function(){
         console.log("Getting ride estimates... from ("+$scope.start_lat+", "+$scope.start_long+") to ("+$scope.end_lat+", "+$scope.end_long+").");
-        $scope.rideEstimates = ComparisonService.getRideEstimates($scope.start_lat, $scope.start_long, $scope.end_lat, $scope.end_long);
-        console.log("got - "+$scope.rideEstimates);
+        ComparisonService.getRideEstimates($scope.start_lat, $scope.start_long, $scope.end_lat, $scope.end_long, $scope);
     ;}
 }]);
--- ride_comparison_webapp/assets/app/services/comparison.js
+++ ride_comparison_webapp/assets/app/services/comparison.js
@@ -1,7 +1,18 @@
-app.service("ComparisonService", [function(){
+app.service("ComparisonService", ["$http", function($http){
     return {
-        getRideEstimates: function(start_lat, start_long, end_lat, end_long){
-            return [2, 4];
+        getRideEstimates: function(start_lat, start_long, end_lat, end_long, scope){
+            var api_domain = "http://localhost:8000";
+            var end_point = "api/rides/estimate/";
+            $http({
+                method: "GET",
+                url: `${api_domain}/${end_point}?start_lat=${start_lat}&start_long=${start_long}&end_lat=${end_lat}&end_long=${end_long}`
+            }).then(function successCallback(response) {
+                console.log(44+scope.rideEstimates);
+                scope.rideEstimates = response.data;
+                console.log(441+scope.rideEstimates);
+            }, function errorCallback(response) {
+                console.log(55+response.statusText);
+            });
         }
     };
 }]);
